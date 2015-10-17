91b5992b88ed534a205932dfecd5ef010b0904d1
Landry Soules <landry.soules@gmail.com>
2015-10-17 16:39:44 +0200

    Define more unit test. Rename specs file.

--- spec/DataBuilder.spec.js
+++ spec/DataBuilder.spec.js
@@ -1,4 +1,4 @@
-describe("Parsing functionnality", function(){
+describe("DataBuilder", function(){
     var requestStub;
     var sinon = require('sinon');
     var chai = require('chai');
@@ -7,11 +7,14 @@ describe("Parsing functionnality", function(){
     var request = require ('request');
     var fs = require('fs');
     var expect = chai.expect;
-    var dataBuilder = require('../lib/DataBuilder');
     var bunyan = require('bunyan');
     var log = bunyan.createLogger({name: 'mocha-test'});
+    var dataBuilder;
+    var nano;
+    var DBUrl = process.env.DB_URL.slice(0);
 
     beforeEach(function(){
+        dataBuilder = require('../lib/DataBuilder');
         requestStub = sinon.stub(request, 'get');
         requestStubPost = sinon.stub(request, 'post');
     });
@@ -46,9 +49,56 @@ describe("Parsing functionnality", function(){
             });
             log.info(lines[0].stations[0]);
             expect(lines.length).to.equals(2);
+            expect(lines[0].stations[0].id).to.equals('206_216_217');
+            expect(lines[0].stations[lines[0].stations.length-1].id).to.equals('206_303_304');
+            expect(lines[1].stations[0].id).to.equals('220_44_64');
+            expect(lines[1].stations[lines[1].stations.length-1].id).to.equals('220_16_91');
             done();
         });
 
+    });
+
+    describe("Database interaction", function(){
+        it("create temp database", function(done){
+
+            var rewire = require('rewire');
+            dataBuilder = rewire('../lib/DataBuilder.js');
+            nano = {db: {create: function(dbName, callback){
+                callback(null, 'fake message');
+            }}};
+            dataBuilder.__set__('nano', nano);
+            log.info(process.env.NODE_ENV);
+            dataBuilder._private.initDB(function(callback){
+                done();
+                log.info('callback:', callback);
+                expect(callback).to.equal('fake message');
+            });
+        });
+
+        it("create design documents");
+
+        it("save lines to database");
+
+        it("remove database");
+
+        it("rename temp database");
+    });
+
+    describe("Configuration", function(){
+
+        beforeEach(function(){
+            delete process.env.DB_URL;
+        });
+
+        afterEach(function(){
+            process.env.DB_URL = DBUrl;
+        });
+
+        it("throws error if a config variable is missing", function(){
+            expect(function(){
+                dataBuilder._private.checkConfig();
+            }).to.throw(Error);
+        });
     });
 
     //it("keep running if a line is in error", function(){
