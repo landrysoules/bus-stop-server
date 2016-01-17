09e282bbb02409ed2e76a349dbadb67f9bdc34f6
Landry Soules <landry.soules@prozone.rs>
2016-01-17 16:39:36 +0100

    Fetch & write lines.
    First population of couch ok.

--- index.js
+++ index.js
@@ -16,6 +16,22 @@ dataBuilder.initDB(function(err, success) {
             log.error('Error while fetching stations', err)
           } else {
             log.info('LINES:', lines)
+            var async = require('async')
+            var bulkDocs = []
+            async.eachSeries(lines, function(line, callback) {
+                dataBuilder.compare(line, bulkDocs, callback)
+              }, function(err) {
+                log.warn('BULK DOCS:', bulkDocs)
+                dataBuilder.save(bulkDocs, function(err,success){
+                  if(err){
+                    log.error(err)
+                  }else{
+                    log.info('SUCCESS !')
+                  }
+                })
+              })
+              // TODO: fetch every line, generate hash, check if present in database, if not, write record and its hashcode.
+              // If present, compare existing hash with new one, if different, update record
           }
         })
       }
